84461ce494967763322243c6c4ed66228230c942
DeepakKumarVarna <mrdeepakvarna@gmail.com>
2020-02-23 16:59:50 +0000

    Improved list of objects by removing column names

--- updateObjects.bat
+++ updateObjects.bat
@@ -10,8 +10,8 @@ Set /P Database="Please enter Database to backup: "
 
 Set /P Object="Please select Objects to Backup: 1. For Procedures 2. For Functions : "
 
-mysql -h %Server% -u %UserName% -p  -e "Select name from mysql.proc where type= 'FUNCTION' AND db='%Database%' " >> ./ObjectList/Functions.txt
-mysql -h %Server% -u %UserName% -p  -e "Select name from mysql.proc where type= 'PROCEDURE' AND db='%Database%' " >> ./ObjectList/Routines.txt
+mysql -N -h %Server% -u %UserName% -p  -e "Select name from mysql.proc where type= 'FUNCTION' AND db='%Database%' " >> ./ObjectList/Functions.txt
+mysql -N -h %Server% -u %UserName% -p  -e "Select name from mysql.proc where type= 'PROCEDURE' AND db='%Database%' " >> ./ObjectList/Routines.txt
 
 
 pause
